d132f16da78a9c863abfdde8c6155af2c52842d9
Fernando de Oliveira <ffernandodeoliveira@gmail.com>
2020-07-07 23:02:46 -0300

    Static file view

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ sudo docker stop <CONTAINER_ID | NAMES>
 sudo docker start <CONTAINER_ID | NAMES>
 
 sudo docker start gostack_postgres_2020
+
+STATIC FILE:
+http://localhost:3333/files/7dbec9eea747bee21852-NEWS_9.28.15_Targets_Fig1.jpg
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -32,7 +33,18 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    return response.json({ ok: true });
+    try {
+      const updateUserAvatarService = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatarService.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
